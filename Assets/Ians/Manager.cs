f08edda7582ad8f167ffa181e4ce90aa109f7e48
Ian Nastajus <ian.nastajus@gmail.com>
2014-07-30 18:26:30 -0400

    PUSH

---
+++ Assets/Ians/Manager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Manager : MonoBehaviour {
+
+    //move to position
+    //control position with mouse
+    //test MoveTowards when hitting colliders in linear path
+    //attack
+    //harvest
+
+    //Camera.main.ScreenToWorldPoint(Input.mousePosition )
+
+    public Transform unitsSelected;
+    public Transform destinationPosition;
+    private Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //right click
+	    if (Input.GetMouseButtonDown(1))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Debug.Log(target);
+            //destinationPosition = 
+
+            //iterate through all children of units Selected and send the new target
+            foreach (Transform unit in unitsSelected)
+            {
+
+            }
+        }
+	}
+}
---
+++ Assets/Ians/Unit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+    public Transform target;
+    private bool movingTowardsTarget = false;
+
+    private float speed = 5f; 
+
+	// Use this for initialization
+	void Start () {
+        if (target != null) 
+        {
+            movingTowardsTarget = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (movingTowardsTarget) 
+        {
+            //if arrives at target then { movingTowardsTarget = false; target = null; }
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        }
+	}
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
